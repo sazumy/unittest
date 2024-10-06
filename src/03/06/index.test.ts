e292b9d0594c086f10f399e0b11f860eca5a1098
Azumi S <azumisaito@gmail.com>
2024-10-06 17:44:07 +0900

    文字列のテストと配列のテストを追加

--- src/03/06/index.test.ts
+++ src/03/06/index.test.ts
@@ -53,4 +53,76 @@ describe("数値の検証", () => {
       expect(0.1 + 0.2).not.toBeCloseTo(0.3, 16); // 小数第16位まで
     });
   });
+
+  describe("文字列の検証", () => {
+    const str = "こんにちは世界";
+    test("検証値 は 期待値 と等しい", () => {
+      expect(str).toBe("こんにちは世界");
+      expect(str).toEqual("こんにちは世界");
+    });
+    test("toContain", () => {
+      expect(str).toContain("世界");
+      expect(str).not.toContain("こんばんは");
+    });
+
+    test("toMatch", () => {
+      expect(str).toMatch(/世界/);
+      expect(str).not.toMatch(/こんばんは/);
+    });
+
+    test("toHaveLength", () => {
+      expect(str).toHaveLength(7);
+      expect(str).not.toHaveLength(8);
+    });
+  });
+
+  describe("オブジェクトの検証", () => {
+    const str = "こんにちは世界";
+    const obj = { status: 200, message: str };
+    test("stringContaining", () => {
+      expect(obj).toEqual({
+        status: 200,
+        message: expect.stringContaining("世界"),
+      });
+    });
+
+    test("stringMatching", () => {
+      expect(obj).toEqual({
+        status: 200,
+        message: expect.stringMatching(/世界/),
+      });
+    });
+  });
+
+  describe("配列の検証", () => {
+    const arr = ["apple", "banana", "cherry"];
+    test("toContain", () => {
+      expect(arr).toContain("banana");
+      expect(arr).not.toContain("grape");
+    });
+
+    test("toHaveLength", () => {
+      expect(arr).toHaveLength(3);
+      expect(arr).not.toHaveLength(4);
+    });
+
+    const obj = { status: 200, message: arr };
+    const obj2 = { status: 200, message: ["apple", "banana", "cherry"] };
+    const obj3 = { status: 200, message: ["apple", "banana", "grape"] };
+    const objects = [obj, obj2];
+    test("toContainEqual", () => {
+      expect(objects).toContainEqual(obj); // 配列に特定のオブジェクトが含まれているか検証する
+      expect(objects).not.toContainEqual(obj3);
+    });
+
+    test("arrayContaining", () => {
+      expect(arr).toEqual(expect.arrayContaining(["banana"]));
+      expect(arr).not.toEqual(expect.arrayContaining(["grape"]));
+    });
+
+    test("arrayContainingEqual", () => {
+      expect(arr).toEqual(expect.arrayContaining(["banana"]));
+      expect(arr).not.toEqual(expect.arrayContaining(["grape"]));
+    });
+  });
 });
