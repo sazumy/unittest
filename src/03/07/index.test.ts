11bbbfb87ebac78cc968c9e9bdf45aa36d92ede7
Azumi S <azumisaito@gmail.com>
2024-10-12 09:49:25 +0900

    非同期処理のテスト続き

--- src/03/07/index.test.ts
+++ src/03/07/index.test.ts
@@ -44,4 +44,31 @@ describe("timeout関数のテスト", () => {
       expect(duration).toBe(50);
     }
   });
+
+  test("指定時間待つと、経過時間をもってrejectする", async () => {
+    try {
+      // timeout関数のつもりが、wait関数を呼び出してしまっている
+      await wait(50);
+      // ここで終了してしまい、テストは成功する
+    } catch (err) {
+      // アサーションは実行されない
+      expect(err).toBe(50);
+    }
+  });
+
+  test("指定時間待つと、経過時間をもってrejectする", async () => {
+    expect.assertions(1); // アサーションが一度実行されることを期待する
+    try {
+      await wait(50);
+      // アサーションが実行されないまま終了するので、テストは失敗する
+    } catch (err) {
+      expect(err).toBe(50);
+    }
+  });
+
+  test("returnしていないため、Promiseが解決される前にテストが終了する", () => {
+    expect(wait(2000)).resolves.toBe(3000);
+    // 正しくは、アサーションをreturnする
+    // return expect(wait(2000)).resolves.toBe(3000);
+  });
 });
