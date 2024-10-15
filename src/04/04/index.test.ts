aeed4d229b7422b588d81c9b418168d42639d631
Azumi S <azumisaito@gmail.com>
2024-10-15 17:52:49 +0900

    モック生成関数を使ったテスト

---
+++ src/04/04/index.test.ts
@@ -0,0 +1,56 @@
+import { getMyArticleLinksByCategory } from ".";
+import * as Fetchers from "../fetchers";
+import { getMyArticlesData, httpError } from "../fetchers/fixtures";
+
+jest.mock("../fetchers");
+
+function mockGetMyArticles(status = 200) {
+  if (status > 299) {
+    // NOTE: spyOnは対象Fetchersの関数をモック化している
+    return jest
+      .spyOn(Fetchers, "getMyArticles")
+      .mockRejectedValueOnce(httpError);
+  }
+
+  return jest
+    .spyOn(Fetchers, "getMyArticles")
+    .mockResolvedValueOnce(getMyArticlesData);
+}
+
+test("データ取得に成功した場合", async () => {
+  mockGetMyArticles();
+});
+
+test("データ取得に失敗した場合", async () => {
+  mockGetMyArticles(500);
+});
+
+test("指定したタグをもつ記事が一件もない場合、null が返る", async () => {
+  mockGetMyArticles();
+  const data = await getMyArticleLinksByCategory("playwright");
+  expect(data).toBeNull();
+});
+
+test("指定したタグをもつ記事が一件以上ある場合、リンク一覧が返る", async () => {
+  mockGetMyArticles();
+  const data = await getMyArticleLinksByCategory("testing");
+  expect(data).toMatchObject([
+    {
+      link: "/articles/howto-testing-with-typescript",
+      title: "TypeScript を使ったテストの書き方",
+    },
+    {
+      link: "/articles/react-component-testing-with-jest",
+      title: "Jest ではじめる React のコンポーネントテスト",
+    },
+  ]);
+});
+
+test("データ取得に失敗した場合、reject される", async () => {
+  mockGetMyArticles(500);
+  await getMyArticleLinksByCategory("testing").catch((err) => {
+    expect(err).toMatchObject({
+      err: { message: "internal server error" },
+    });
+  });
+});
