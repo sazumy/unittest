514c657d65ad37450ee30cc1e0ce64899a5a614f
Azumi S <azumisaito@gmail.com>
2025-03-12 17:22:36 +0900

    5-4

---
+++ src/05/04/ArticleListItem.tsx
@@ -0,0 +1,14 @@
+export interface ItemProps {
+  id: string;
+  title: string;
+  body: string;
+}
+export const ArticleListItem = ({ id, title, body }: ItemProps) => {
+  return (
+    <li>
+      <h3>{title}</h3>
+      <p>{body}</p>
+      <a href={`/articles/${id}`}>もっと見る</a>
+    </li>
+  )
+};
